021f5d02db2657334df8aef1b9d6dcf6c0877d49
aslamcontact <aslam.mycontact@gmail.com>
2023-09-26 22:13:18 +0530

    setup project for product images

---
+++ src/main/java/com/ecom/product_images/ProductImagesApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.product_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductImagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/product_images/ProductImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.product_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
