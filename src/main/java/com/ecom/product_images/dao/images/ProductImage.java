17f02d56a7413b5d031468af17a2e4bf57600add
aslamcontact <aslam.mycontact@gmail.com>
2023-09-27 20:30:13 +0530

    add productImage  class to store image using  lob object on mysql

---
+++ src/main/java/com/ecom/product_images/dao/images/ProductImage.java
@@ -0,0 +1,36 @@
+package com.ecom.product_images.dao.images;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Embeddable
+
+
+public class ProductImage {
+
+
+
+    @Lob
+    @Column(length = 20971520)
+    byte[] image;
+
+    public ProductImage() {
+    }
+
+    public ProductImage( byte[] image) {
+
+        this.image = image;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+   }
