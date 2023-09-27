c7784f2021c646034b3bb94ed0ba8cd6094a931a
aslamcontact <aslam.mycontact@gmail.com>
2023-09-28 00:54:19 +0530

    add get and post method for image

--- src/main/java/com/ecom/product_images/api/ControllerImage.java
+++ src/main/java/com/ecom/product_images/api/ControllerImage.java
@@ -2,20 +2,37 @@ package com.ecom.product_images.api;
 
 import com.ecom.product_images.servicelayer.ProductImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ControllerImage {
     @Autowired
         ProductImageService productImageService;
-    @GetMapping("/image")
+    @PostMapping("/product/mapper")
     public String createImageMapper(@RequestParam String product)
     {
 
         return productImageService.createImageMapper(product);
+    }
+    @PostMapping("/product/mapper/image")
+    public String addImageToMapper(@RequestParam String product,
+                                   @RequestParam String category,
+                                   @RequestBody MultipartFile image
+                                   ) throws IOException {
+        return productImageService.addImageToMapper(product,category,image.getBytes());
+    }
+    @GetMapping( value = "/product/mapper/image",
+                 produces = MediaType.IMAGE_JPEG_VALUE
+                )
+    public byte[] getImage(@RequestParam String product,
+                           @RequestParam String category)
+    {
+
+      return productImageService.getImage(product,category);
     }
 }
--- src/main/java/com/ecom/product_images/servicelayer/ProductImageService.java
+++ src/main/java/com/ecom/product_images/servicelayer/ProductImageService.java
@@ -1,9 +1,12 @@
 package com.ecom.product_images.servicelayer;
 
+import com.ecom.product_images.dao.images.ProductImage;
 import com.ecom.product_images.dao.product.ProductImageMapper;
 import com.ecom.product_images.dao.product.ProductImageMapperRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductImageService {
@@ -17,6 +20,29 @@ public class ProductImageService {
        productImageMapperRepo.save(imageMapper);
         return imageMapper.getProductImagesId();
     }
+
+    public String addImageToMapper(String productName,
+                                   String imageType,
+                                   byte[] imageByte)
+    {
+     Optional<ProductImageMapper> product= productImageMapperRepo.findById(productName);
+        if(product.isEmpty())
+            throw new IllegalStateException("there is no product in database");
+        product.get().getImages().put(imageType,new ProductImage(imageByte));
+       return productImageMapperRepo.save(product.get()).getProductImagesId();
+    }
+
+    public byte[] getImage(
+                     String product,
+                     String category
+                    )
+    {
+        Optional<ProductImageMapper> result;
+        result=productImageMapperRepo.findById(product);
+        if(result.isEmpty())throw  new IllegalStateException("there is no product");
+        return result.get().getImages().get(category).getImage();
+    }
+
 
 
 }
